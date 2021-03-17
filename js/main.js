2eb53dfaf80cafb221b3a0ab85321f37f9b40e9a
Mint <reginaliew9@gmail.com>
2021-03-18 01:58:43 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,65 @@
 $(document).ready(function(){
     var numOfInput = 0;
     var choices = new Array();
+    var removed = new Array();
+    var removedId = new Array();
     var color = ["is-error", "is-warning", "is-success", "is-dark", "is-primary"];
+    
+    $(document).on("click", "span.choices", function(event){
+        var choiceId = this.id;
+        removedId.push(choiceId);
+        removed.push(choices[choiceId]);
+        $("#"+choiceId).remove();
+    });
+
     $("#add").on("click", function(){
         var tempVal = $("input[name=choice]").val();
         if (tempVal.length == 0) {
             document.getElementById('dialog-empty-input').showModal();
         } else {
-            choices.push(tempVal);
-            $("input[name=choice]").val("");
-            var newChoice = '<span class="nes-badge"><span class="'+color[Math.floor(Math.random()*color.length)]+'">'+tempVal+'</span></span>';
-            $(newChoice).appendTo(".block");
+            var similar = 0;
+            for(var i=0;i<choices.length;i++){
+                if(tempVal===choices[i]) {
+                    similar++;
+                }
+            }
+            if (similar<=0) {
+                choices.push(tempVal);
+                $("input[name=choice]").val("");
+                var newChoice = '<span class="nes-badge choices" id="'+numOfInput+'"><span class="'+color[Math.floor(Math.random()*color.length)]+'">'+tempVal+'</span></span>';
+                $(newChoice).appendTo(".block");
+                numOfInput++;
+            } else {
+                document.getElementById('dialog-same-input').showModal();
+            }
         }
     });
+
     var interval = null;
     $("#start").on("click", function(){
-        interval = setInterval(randomize, 50);
-        setTimeout(function(){
-            clearInterval(interval);
-            $('<span style="padding-bottom: 10px;"><i class="nes-icon trophy is-large"></i></span>').insertBefore('.randomize span');
-        }, 4000)
+        if(choices.length>1){
+            document.getElementById('dialog-show-result').showModal();
+            $("#result").css("opacity", "1");
+            interval = setInterval(randomize, 50);
+            setTimeout(function(){
+                clearInterval(interval);
+                $('<span style="padding-bottom: 10px;"><i class="nes-icon trophy is-large"></i></span>').insertBefore('.randomize span');
+            }, 4000)
+        }else{
+            document.getElementById('dialog-start-fail').showModal();
+        }
     });
+
     function randomize() {
+        var tempArr = new Array();
+        tempArr = choices.filter(function(val) {
+            return removed.indexOf(val) == -1;
+        });
+        console.log(tempArr);
         var rand = 0;
-        rand = Math.floor(Math.random()*choices.length);
+        rand = Math.floor(Math.random()*tempArr.length);
         $(".randomize").empty();
-        $('<span>'+choices[rand]+'</span>').appendTo(".randomize");
+        $('<span>'+tempArr[rand]+'</span>').appendTo(".randomize");
         console.log(rand);
     }
     // var numOfInput = 1;
